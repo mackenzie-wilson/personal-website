7ab9cab4921e08ae1f73be2956c4d079dae0bb2e
mackenzie-wilson <mackenzie@macwilson.tech>
2025-04-14 21:28:22 -0400

    Fix ThemeProvider export to default to resolve build error

--- src/app/components/ThemeProvider.tsx
+++ src/app/components/ThemeProvider.tsx
@@ -2,10 +2,11 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider defaultTheme="light" {...props}>
       {children}
     </NextThemesProvider>
   )
 }
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "../../styles/globals.css"
-import { ThemeProvider } from "./components/ThemeProvider"
+//import { ThemeProvider } from "./components/ThemeProvider"
+import ThemeProvider from "./components/ThemeProvider"
 
 const inter = Inter({ subsets: ["latin"] })
 
